1ff93600211d155bd5dc831207aad5a2a033b081
Hugo Devillers <hugo@xol.io>
2022-09-18 19:27:04 +0200

    added some path sanitation

--- src/common/io.c
+++ src/common/io.c
@@ -1,3 +1,4 @@
+#include "cunk/print.h"
 #include "common_private.h"
 
 #include <stdlib.h>
@@ -6,6 +7,21 @@
 // this does not work on non-POSIX compliant systems
 // Cygwin/MINGW works though.
 #include "sys/stat.h"
+
+static const char* sanitize_path(const char* path) {
+    size_t pathlen = strlen(path);
+    bool windows_path = false;
+    if (pathlen > 2) {
+        if (path[1] == ':')
+            windows_path = true;
+    }
+
+    if (windows_path) {
+        return replace_string(path, "/", "\\");
+    }
+
+    return format_string("%s", path);
+}
 
 bool read_file(const char* filename, size_t* out_size, char** out_buffer) {
 	char* buffer = NULL;
@@ -29,10 +45,13 @@ bool read_file(const char* filename, size_t* out_size, char** out_buffer) {
 }
 
 bool file_exists(const char* filename) {
+    const char* sanitized = sanitize_path(filename);
     struct stat s = { 0 };
     if (stat(filename, &s) == 0) {
+        free((char*) sanitized);
         return true;
     }
+    free((char*) sanitized);
     return false;
 }
 
