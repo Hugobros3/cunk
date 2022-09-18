549242b4001c6019d7876a35305627f4e05c5974
Hugo Devillers <hugo@xol.io>
2022-09-18 19:30:04 +0200

    added some bit utils

--- src/common/test_common_misc.c
+++ src/common/test_common_misc.c
@@ -1,7 +1,17 @@
 #include "common_private.h"
 #include <assert.h>
+#include <stdlib.h>
+
+static void test_bit_fetches() {
+    int64_t arr[] = { 0, 1, 2, 3, 0xDEADBEEF };
+    int64_t r = fetch_bits(arr, sizeof(int64_t) * 4 * CHAR_BIT, 64);
+    assert(r == 0xDEADBEEF);
+    r = fetch_bits(arr, sizeof(int64_t) * 4 * CHAR_BIT + 4, 60);
+    assert(r == 0x0DEADBEE);
+}
 
 int main() {
     assert(swap_endianness(8, 0x0011223344556677) == 0x7766554433221100 && "");
+    test_bit_fetches();
     return 0;
 }
--- src/common/util.c
+++ src/common/util.c
@@ -1,5 +1,6 @@
 #include "common_private.h"
 
+#include <stdlib.h>
 #include <assert.h>
 #include <string.h>
 
@@ -24,4 +25,53 @@ int64_t swap_endianness(int bytes, int64_t i) {
     for (int byte = 0; byte < bytes; byte++)
         acc |= ((i >> byte * 8) & 0xFF) << (bytes - 1 - byte) * 8;
     return acc;
+}
+
+unsigned int needed_bits(unsigned int n) {
+    if (n == 0)
+        return 0;
+    int bits = 1;
+    int maxn = 2;
+    while (n > maxn) {
+        bits++;
+        maxn *= 2;
+    }
+    return bits;
+}
+
+uint64_t fetch_bits(const char* buf, size_t bit_pos, unsigned int width) {
+    int64_t acc = 0;
+    size_t pos = SIZE_MAX;
+    char last_fetch;
+    for (size_t bit = 0; bit < width; bit++) {
+        size_t new_pos = (bit_pos + bit) / CHAR_BIT;
+        if (new_pos != pos) {
+            pos = new_pos;
+            last_fetch = buf[pos];
+        }
+        int64_t b = (last_fetch >> ((bit_pos + bit) & 0x7)) & 0x1;
+        acc |= (b << bit);
+    }
+    return acc;
+}
+
+uint64_t fetch_bits_long_arr(const uint64_t* buf, bool big_endian, size_t bit_pos, unsigned int width) {
+#define LONG_BITS (CHAR_BIT * sizeof(int64_t))
+    int64_t acc = 0;
+
+    size_t pos = SIZE_MAX;
+    uint64_t last_fetch;
+    for (size_t bit = 0; bit < width; bit++) {
+        size_t new_pos = (bit_pos + bit) / LONG_BITS;
+        if (new_pos != pos) {
+            pos = new_pos;
+            last_fetch = buf[pos];
+            if (big_endian)
+                last_fetch = swap_endianness(8, last_fetch);
+        }
+        uint64_t b = (last_fetch >> ((bit_pos + bit) & (LONG_BITS - 1))) & 0x1;
+        acc |= (b << bit);
+    }
+    return acc;
+#undef LONG_BITS
 }
